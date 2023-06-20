9444b0ffdda377714fa31c6c5dbf67102b164951
Julio Cesar Chacon Vilela <julio.chacon@neoris.com>
2023-06-20 11:42:50 -0500

    Se establece la base del proyecto

--- 02-react-app/src/01-lazyload/pages/NoLazy.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export const NoLazy = () => {
-    return (
-        <h1>NoLazy Component</h1>
-    )
-}
--- 02-react-app/src/routes/Navigation.tsx
+++ 02-react-app/src/routes/Navigation.tsx
@@ -1,46 +1,37 @@
-import { Suspense } from 'react';
-import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 
-import logo from '../logo.svg';
-import { routes } from "./routes";
+import logo from '../logo.svg'
 
 export const Navigation = () => {
     return (
-        <Suspense fallback={<span>Loading ...</span>}>
-            <BrowserRouter>
-                <div className="main-layout">
-                    <nav>
-                        <img src={logo} alt="React Logo" />
-                        <ul>
-                            {
-                                routes.map(route => (
-                                    <li key={route.to}>
-                                        <NavLink
-                                            to={route.to}
-                                            className={({ isActive }) => isActive ? 'nav-active' : ''}
-                                        >
-                                            {route.name}
-                                        </NavLink>
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </nav>
+        <BrowserRouter>
+            <div className="main-layout">
+                <nav>
+                    <img src={ logo } alt="React Logo" />
+                    <ul>
+                        <li>
+                            <NavLink to="/home" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/users" className={ ({ isActive }) => isActive ? 'nav-active' : '' }>Users</NavLink>
+                        </li>
+                    </ul>
+                </nav>
 
-                    <Routes>
-                        {
-                            routes.map(route => (
-                                <Route
-                                    key={route.path}
-                                    path={route.path}
-                                    element={<route.Component />}
-                                />
-                            ))
-                        }
-                        <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </Suspense>
+
+                <Routes>
+                    <Route path="about" element={ <h1>About Page</h1> } />
+                    <Route path="users" element={ <h1>Users Page</h1> } />
+                    <Route path="home" element={ <h1>Home Page</h1> } />
+                    
+                    <Route path="/*" element={ <Navigate to="/home" replace /> } />
+                </Routes>
+
+            </div>
+        </BrowserRouter>
     )
 }
